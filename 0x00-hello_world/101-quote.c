630d342a2f975ed5c4db8b005a82f38b9a8070c1
LarrysGraphix <ljeremiah131@gmail.com>
2023-06-19 15:55:11 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,9 @@
  */
 int main(void)
 {
-char message[] = "and that piece of art is useful\"Dora Korpar, 2015-10-19\n";
-write(STDOUT_FILENO, message, sizeof(message) - 1);
+char message[] = "and that piece of art is useful\" -  ";
+char message[] = "Dora Korpar, 2015-10-19\n";
+write(STDOUT_FILENO, message1, sizeof(message1) 1);
+write(STDOUT_FILENO, message2, sizeof(message2) 1);
 return (1);
 }
