2779434f92d9310f18538e1ecff639fc0be0ef1b
LarrysGraphix <ljeremiah131@gmail.com>
2023-06-19 06:46:09 -0700

    Right Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 /**
- * main - Entry point (A program that prints the size of various types on the computer)
+ * main - Entry point (A program that prints the size.)
  * Return: 0 (Indicates Success)
  */
-int main (void)
+int main(void)
 {
 char c;
 int i;
 long int r;
 long long int d;
 float f;
-
-printf("Size of a char: %zu bytes(s)\n", (unsigned) sizeof(c));
-printf("Size of an int: %zu bytes(s)\n", (unsigned) sizeof(i));
-printf("Size of a long int: %zu bytes(s)\n", (unsigned) sizeof(r));
-printf("Size of a long long int: %zu bytes(s)\n", (unsigned) sizeof(d));
-printf("Size of a float: %zu bytes(s)\n", (unsigned) sizeof(f));
+printf("Size of a char: %zu bytes(s)\n", (unsigned int)sizeof(c));
+printf("Size of an int: %zu bytes(s)\n", (unsigned int)sizeof(i));
+printf("Size of a long int: %zu bytes(s)\n", (unsigned int)sizeof(r));
+printf("Size of a long long int: %zu bytes(s)\n", (unsigned int)sizeof(d));
+printf("Size of a float: %zu bytes(s)\n", (unsigned int)sizeof(f));
 return (0);
 }
