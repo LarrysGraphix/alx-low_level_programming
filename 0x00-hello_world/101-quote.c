854fca38a27057416a049c9671ae50ee0f41c773
LarrysGraphix <ljeremiah131@gmail.com>
2023-06-19 10:18:56 -0700

    Qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	putf("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout)	
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+	write(STDOUT_FILENO, message, sizeof(message) - 1);	
 	return (1);
 }
