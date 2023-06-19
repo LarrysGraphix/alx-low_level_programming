42287e8399c1608b997850fd432d19ba026942a8
LarrysGraphix <ljeremiah131@gmail.com>
2023-06-19 15:29:27 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point (A program that prints a line of code using simple function)
- *Return: 1 (Set return)
+ * main - Entry Point(A simple program)
+ * Return: 1(Set)
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
-	write(STDOUT_FILENO, message, sizeof(message) - 1);	
-	return (1);
+char message[] = "and that piece of art is useful\"Dora Korpar, 2015-10-19\n";
+write(STDOUT_FILENO, message, sizeof(message) - 1);
+return (1);
 }
